2ea7b91c8cf0c54f28aa71e5614d5b70e428b644
LegionModding <legion@legionmodding.com>
2022-01-29 18:01:01 +0000

    Fix more build errors

--- src/main/java/com/legionmodding/classictinkers/ClassicTinkers.java
+++ src/main/java/com/legionmodding/classictinkers/ClassicTinkers.java
@@ -14,20 +14,17 @@ public class ClassicTinkers
 {
     public static final String MOD_ID = "classictinkers";
 
-/*    public static final ItemGroup CREATIVE_TAB_BLOCKS = new ItemGroup("creativetab")
+    public static final ItemGroup CREATIVE_TAB_BLOCKS = new ItemGroup("creativetab")
     {
         @Override
         public ItemStack makeIcon()
         {
             return new ItemStack(BlockRegistry.CASTING_BASIN.get());
         }
-    };*/
+    };
 
     public ClassicTinkers()
     {
-        //BlockRegistry.registerBlocks();
-        //ItemRegistry.registerItems();
-
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onCommonSetup);
 
--- src/main/java/com/legionmodding/classictinkers/handler/registry/ItemRegistry.java
+++ src/main/java/com/legionmodding/classictinkers/handler/registry/ItemRegistry.java
@@ -13,8 +13,8 @@ public class ItemRegistry
 {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ClassicTinkers.MOD_ID);
 
-    private static final RegistryObject<Item> CASTING_BASIN = ITEMS.register("casting_basin", () -> new BlockItem(BlockRegistry.CASTING_BASIN.get(), new Item.Properties().tab(ClassicTinkers.CREATIVE_TAB_BLOCKS)));
-    private static final RegistryObject<Item> CASTING_TABLE = ITEMS.register("casting_table", () -> new BlockItem(BlockRegistry.CASTING_TABLE.get(), new Item.Properties().tab(ClassicTinkers.CREATIVE_TAB_BLOCKS)));
+    //private static final RegistryObject<Item> CASTING_BASIN = ITEMS.register("casting_basin", () -> new BlockItem(BlockRegistry.CASTING_BASIN.get(), new Item.Properties().tab(ClassicTinkers.CREATIVE_TAB_BLOCKS)));
+    //private static final RegistryObject<Item> CASTING_TABLE = ITEMS.register("casting_table", () -> new BlockItem(BlockRegistry.CASTING_TABLE.get(), new Item.Properties().tab(ClassicTinkers.CREATIVE_TAB_BLOCKS)));
 
     public static void registerItems()
     {
